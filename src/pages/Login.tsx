918f8828968d63040827ef76999c273e549e90b9
Lorendacol <lorendacol@gmail.com>
2025-03-07 11:17:43 +0800

    Added new features to login.tsx and adding changes in vite.config.ts

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -3,35 +3,48 @@ import {
     IonButtons,
     IonContent,
     IonHeader,
+    IonIcon,
+    IonInput,
+    IonItem,
     IonMenuButton,
     IonPage,
     IonTitle,
     IonToolbar,
     useIonRouter
   } from '@ionic/react';
+  import { personCircleOutline } from 'ionicons/icons';
   function Login() {
     const navigation = useIonRouter();
     const doLogin = () => {
       navigation.push('/it35-lab/app', 'forward', 'replace');
     };
-  
-      return (
-        <IonPage>
-          <IonHeader>
-            <IonToolbar>
-              <IonButtons slot='start'>
-                <IonMenuButton></IonMenuButton>
-              </IonButtons>
-              <IonTitle>Login</IonTitle>
-            </IonToolbar>
-          </IonHeader>
-          <IonContent className='ion-padding'>
-            <IonButton onClick={() => doLogin()} expand="full">
-              Login
-            </IonButton>
-          </IonContent>
-        </IonPage>
-      );
-    }
+    
+    return (
+      <IonPage>
+        <IonHeader>
+          <IonToolbar>
+            <IonButtons slot='start'>
+              <IonMenuButton></IonMenuButton>
+            </IonButtons>
+            <IonTitle>Login</IonTitle>
+          </IonToolbar>
+        </IonHeader>
+        <IonContent className='ion-padding'>
+        <div style={{ textAlign: 'center', marginBottom: '20px' }}>
+          <IonIcon icon={personCircleOutline} style={{ fontSize: '80px' }} />
+        </div>
+        <IonItem>
+          <IonInput label="Username" placeholder="Enter Username"></IonInput>
+        </IonItem>
+        <IonItem>
+          <IonInput label="Password" type="password" placeholder="Enter Password"></IonInput>
+        </IonItem>
+          <IonButton onClick={() => doLogin()} expand="full">
+            Login
+          </IonButton>
+        </IonContent>
+      </IonPage>
+    );
+  }
     
     export default Login;
