bb1e1173be10d6a695c1ec09c299e8f1401ebfbf
Pinton04 <robertpasukin042518@gmail.com>
2025-03-25 08:45:43 +0800

    added register

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -42,6 +42,9 @@ import {
           <IonButton onClick={() => doLogin()} expand="full">
             Login
           </IonButton>
+          <IonButton routerLink="/it35-lab/register" expand="full" fill="clear" shape='round'>
+                 Don't have an account? Register here
+             </IonButton>
         </IonContent>
       </IonPage>
     );
