eec28e6e7c742bb62bb8345020507e39aaf70f80
Pinton04 <robertpasukin042518@gmail.com>
2025-03-25 08:50:23 +0800

    Updates

--- assets/ios.transition-legacy-DODiCnv7.js
+++ assets/ios.transition-legacy-DODiCnv7.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-BLPXPnAS.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.m,n=t.n}],execute:function(){
+System.register(["./index-legacy-C6sdmTax.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.n,n=t.o}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
